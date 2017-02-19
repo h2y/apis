f729d5be2b1028d9affba6bbdd7a7f431000d697
Moshel <hzy961016@163.com>
2017-02-19 13:18:06 +0800

    small enhance

--- scripts/api-start.bat
+++ scripts/api-start.bat
@@ -3,6 +3,8 @@ cd ..
 git fetch --all
 git reset --hard origin/master
 
+cnpm i
+
 npm run start
 
 pause
--- scripts/api-start.sh
+++ scripts/api-start.sh
@@ -5,4 +5,6 @@ cd ..
 git fetch --all
 git reset --hard origin/master
 
+cnpm i 
+
 npm run start
